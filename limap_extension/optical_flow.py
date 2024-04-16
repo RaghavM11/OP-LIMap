58cfcb270259be32227fd679f77e4bbd63d811ed
shlok <ashlok@umich.edu>
2024-04-16 14:32:06 -0400

    opt Flow

--- limap_extension/optical_flow.py
+++ limap_extension/optical_flow.py
@@ -6,7 +6,9 @@ import cv2
 import numpy as np
 import torch
 from scipy.ndimage import binary_dilation
-
+from scipy.interpolate import griddata
+from PIL import Image
+import matplotlib.pyplot as plt
 from limap_extension.raft.raft import RAFT
 from limap_extension.utils import flow_viz
 from limap_extension.utils.utils import InputPadder
@@ -14,6 +16,14 @@ from limap_extension.img_cloud_transforms import reproject_img
 from limap_extension.transforms_spatial import get_transform_matrix_from_pose_array
 
 
+def interpolateImage(image, samples):
+    ix = np.random.randint(image.shape[1], size= samples)
+    iy = np.random.randint(image.shape[0], size= samples)
+    samples = image[iy, ix]
+    nx, ny = image.shape[1], image.shape[0]
+    X, Y = np.meshgrid(np.arange(0, nx, 1), np.arange(0, ny, 1))
+    interpim = griddata((iy, ix), samples, (Y, X))
+    return interpim
 class Args():
 
     def __init__(self, model='', path='', small=False, mixed_precision=True, alternate_corr=False):
@@ -88,15 +98,21 @@ def motion_segmentation(rgb_1: np.ndarray, depth_1: np.ndarray, rgb_2: np.ndarra
     pose_1 = get_transform_matrix_from_pose_array(cp1)
     pose_2 = get_transform_matrix_from_pose_array(cp2)
 
-    # Reproject the image at time t to the image frame at time t+1
-    img_1_in_frame_2, valid_bbox = reproject_img(rgb_1, depth_1, pose_1, pose_2)
 
-    # Crop the images to the valid bounding box
-    img_1_in_frame_2 = valid_bbox.crop_img(img_1_in_frame_2)
-    rgb_2 = valid_bbox.crop_img(rgb_2)
+    # Reproject the image at time t to the image frame at time t+1
+    img_1_in_frame_2 = reproject_img(rgb_1, depth_1, pose_1, pose_2)
+    # grey_1_in_frame_2 = cv2.cvtColor(img_1_in_frame_2, cv2.COLOR_RGB2GRAY)
+    # grey_img_1_in_frame_2 = interpolateImage(grey_1_in_frame_2, 1000000)
+    # plt.figure()
+    # plt.imshow(grey_img_1_in_frame_2, cmap='gray')
+    # plt.show()
+    # raise exception
+    # img_1_in_frame_2 = cv2.cvtColor(grey_img_1_in_frame_2, cv2.COLOR_GRAY2RGB)
 
     # Compute the optical flow between the two images in the same frame to determine dynamic
     # objects.
+    blur_2 = cv2.GaussianBlur(rgb_2, dst = None, ksize =(3, 3), sigmaX=5)
+    blur_1 = cv2.GaussianBlur(img_1_in_frame_2, dst = None, ksize =(3, 3), sigmaX=5)
     flow_low, flow_up = flow.infer_flow(rgb_2, img_1_in_frame_2)
     # print(flow_up, flow_low)
     flow_up = flow_up[0].permute(1, 2, 0).cpu().numpy()
@@ -105,18 +121,26 @@ def motion_segmentation(rgb_1: np.ndarray, depth_1: np.ndarray, rgb_2: np.ndarra
     mag, angle = cv2.cartToPolar(flow_up[..., 0], flow_up[..., 1], angleInDegrees=True)
 
     mask[:, :, 0] = 0
+    mag = np.sqrt(mag**2 + depth_2**2)
     mask[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
-
+    print(np.mean(mag), np.max(mag), np.min(mag))
+    # mag2 = (mag- np.mean(mag))/np.std(mag)
+    # motion_mask = np.uint8(mag2 < np.mean(mag2)) * 255 
+    # kernel = np.ones((7,7))
+    # motion_mask = cv2.erode(motion_mask, kernel, iterations=1)
+    # motion_mask = cv2.morphologyEx(motion_mask, cv2.MORPH_OPEN, kernel, iterations=1)
+    # motion_mask = cv2.morphologyEx(motion_mask, cv2.MORPH_CLOSE, kernel, iterations=3)
     mask_rgb = cv2.cvtColor(mask, cv2.COLOR_HSV2RGB)
     mask_grey = cv2.cvtColor(mask_rgb, cv2.COLOR_RGB2GRAY)
     mean = np.mean(mask_grey)
-    print(mean)
+    # # print(mean)
     std_deviation = np.std(mask_grey)
-    print(std_deviation)
+    # # print(std_deviation)
     mask_grey = (mask_grey - mean) / std_deviation
-    print(np.mean(mask_grey))
+    # # mask_grey = cv2.normalize(mask_grey, None, 0, 1, cv2.NORM_MINMAX)
     mask_grey = np.where(mask_grey < 0, 0, mask_grey)
-    mask_grey = np.where(mask_grey > 0.5, 1, 0)
+    mask_grey = np.where(mask_grey > 0.9, 1, 0)
+    # # print(np.mean(mask_grey[np.where(mask_grey > 0)]))
     mask_r = np.mean(mask_rgb[:, :, 0])
     mask_g = np.mean(mask_rgb[:, :, 1])
     mask_b = np.mean(mask_rgb[:, :, 2])
@@ -127,6 +151,8 @@ def motion_segmentation(rgb_1: np.ndarray, depth_1: np.ndarray, rgb_2: np.ndarra
     mask_g = (mask_rgb[:, :, 1] - mask_g) / cov_mask_g
     mask_b = (mask_rgb[:, :, 2] - mask_b) / cov_mask_b
     mask_rgb = np.stack([mask_r, mask_g, mask_b], axis=2)
+    # mask_grey = cv2.cvtColor(mask_rgb*255, cv2.COLOR_RGB2GRAY)
+    # mask_grey =  mask_grey * 255
 
     # do binary dilation on the grey_mask
     mask_grey = binary_dilation(mask_grey, iterations=5)
@@ -134,13 +160,82 @@ def motion_segmentation(rgb_1: np.ndarray, depth_1: np.ndarray, rgb_2: np.ndarra
     # mask = np.asarray(mask_rgb[..., 2], dtype=np.uint8)
     # mask[mask < threshold] =
     # mask[mask >= threshold] = 1
-
+    
+    
     # Save it
 
-    # plt.figure()
-    # plt.imshow(mask_grey, cmap='gray')
-    # plt.show()
+    plt.figure()
+    plt.imshow(mask_rgb)
+
+    plt.figure()
+    plt.imshow(img_1_in_frame_2)
+
+    plt.figure()
+    plt.imshow(rgb_2)
+
+    plt.figure()
+    plt.imshow(mask_grey, cmap='gray')
+    plt.show()
     return mask_rgb, mask_grey
 
 
-# NOTE: Moved the runner code to scripts/run_flow_example.py
+
+if __name__ == '__main__':
+    ten = torch.tensor([1, 2, 3, 4, 5])
+    print(ten.device)
+    path = '/Users/shlokagarwal/Desktop/Mobile Robotics/project/LIMap-Extension/raft-sintel.pth'
+    flow = OpticalFlow(None)
+    flow.load_model(path, Args())
+    for i in range(357):
+        j = i+1
+        if ( j > 356):
+            break
+        path1 = '/Users/shlokagarwal/Desktop/Mobile Robotics/project/LIMap-Extension/datasets/P006/image_left/'
+        path2 = '/Users/shlokagarwal/Desktop/Mobile Robotics/project/LIMap-Extension/datasets/P006/depth_left/'
+        path5 = '/Users/shlokagarwal/Desktop/Mobile Robotics/project/LIMap-Extension/datasets/P006/pose_left.txt'
+        if i < 10:
+            img_num = '00000' + str(i)
+        elif i < 100:
+            img_num = '0000' + str(i)
+        else:
+            img_num = '000' + str(i)
+        if j < 10:
+            img_num2 = '00000' + str(j)
+        elif j < 100:
+            img_num2 = '0000' + str(j)
+        else:
+            img_num2 = '000' + str(j)
+        img_num = '000091'
+        img_num2 = '000092'
+        fr1 = np.array(
+            Image.open(
+                path1 + img_num + '_left.png'
+            ))
+        fr2 = np.array(
+            Image.open(
+                path1 + img_num2 + '_left.png'
+            ))
+        depth1 = np.load(path2 + img_num + '_left_depth.npy')
+        depth2 = np.load(path2 + img_num2 + '_left_depth.npy')
+
+        cam_pose = np.loadtxt(path5)
+        # print(cam_pose.shape)
+        # flow_viz = flow.visualize(flow_up)
+        # flow_viz = cv2.cvtColor(flow_viz, cv2.COLOR_RGB2BGR)
+        # flow_viz = np.concatenate([fr1, flow_viz], axis=0)
+        mask_rgb, mask_grey = motion_segmentation(fr1, depth1, fr2, depth2, cam_pose[i, : ], cam_pose[j, : ])
+
+        # save the masks as png
+        # plt.figure() 
+        # plt.imshow(mask_grey)
+        # plt.figure()
+        # plt.imshow(mask_rgb)
+        # plt.figure()
+        # plt.imshow(fr1)
+        # plt.figure()
+        # plt.imshow(fr2)
+        # plt.show()
+        cv2.imwrite('/Users/shlokagarwal/Desktop/Mobile Robotics/project/LIMap-Extension/datasets/P007/masks_left/' + img_num + '_mask.png', mask_grey)
+        break
+    # cv2.imshow('flow', flow_viz / 255.0)
+    # cv2.waitKey()
