1e217daa28e4439a0f3966c2ae7bd6d3e7a031ec
Dylan Colli <dylanfrankcolli@gmail.com>
2024-04-17 05:52:46 -0400

    Correctly transform point clouds
    - I realized I wasn't doing the orthogonal transform for swapping axes between NED and tartan conventions

--- limap_extension/constants.py
+++ limap_extension/constants.py
@@ -4,7 +4,7 @@ import numpy as np
 CAM_INTRINSIC = np.array((
     (320,   0, 320),
     (  0, 320, 240),
-    (  0,   0,   1)))
+    (  0,   0,   1)), dtype=float)
 
 # H_IMG_TO_CAM = np.array((
 #     (0, 0, -1, 0),
@@ -12,10 +12,23 @@ CAM_INTRINSIC = np.array((
 #     (-1, 0, 0, 0),
 #     (0, 0, 0, 1)
 # ))
-H_IMG_TO_CAM = np.array((
-    (1, 0, 0, 0),
+# H_IMG_TO_CAM = np.array((
+#     (0, 0, 1, 0),
+#     (1, 0, 0, 0),
+#     (0, 1, 0, 0),
+#     (0, 0, 0, 1)
+# ))
+H_CAM_TO_NED = np.array((
     (0, 1, 0, 0),
     (0, 0, 1, 0),
+    (1, 0, 0, 0),
     (0, 0, 0, 1)
-))
+), dtype=np.float32)
+H_NED_TO_CAM = np.array((
+    (0, 0, 1, 0),
+    (1, 0, 0, 0),
+    (0, 1, 0, 0),
+    (0, 0, 0, 1)
+), dtype=np.float32)
+# H_IMG_TO_CAM = np.eye(4)
 # yapf: enable
