4c847e3af874ff2024d47f7b54abdb489b7708d2
Dylan Colli <dylanfrankcolli@gmail.com>
2024-04-16 21:46:52 -0400

    Fixes transforms
    - I always think I can hack this transform stuff but it bites me in the end.

--- limap_extension/img_cloud_transforms.py
+++ limap_extension/img_cloud_transforms.py
@@ -5,7 +5,7 @@ import numpy as np
 import torch
 # import cv2
 
-from limap_extension.constants import CAM_INTRINSIC
+from limap_extension.constants import CAM_INTRINSIC, H_IMG_TO_CAM
 from limap_extension.bounding_box import BoundingBox
 from limap_extension.point_cloud import PointCloud
 
@@ -15,10 +15,78 @@ METERS_TO_METERS = 1.0
 
 
 def get_uv_coords(img_rows, img_cols):
-    V, U = np.meshgrid(np.arange(img_rows), np.arange(img_cols))
+    U, V = np.meshgrid(np.arange(img_rows), np.arange(img_cols))
     V = V.T.flatten()
     U = U.T.flatten()
     return U, V
+
+
+def augment_coords(coords: np.ndarray):
+    if ((not coords.shape[1] == 3) or (not coords.shape[1] != 2)):
+        raise ValueError(f"Coords must have 2 or 3 columns, got shape {coords.shape}")
+    return np.hstack([coords, np.ones((coords.shape[0], 1))])
+
+
+def tform_coords(tform: np.ndarray, coords: np.ndarray) -> np.ndarray:
+    coords_aug = augment_coords(coords)
+    return (tform @ coords_aug.T).T[:, :-1]
+
+
+def uvz_to_xyz(us: np.ndarray,
+               vs: np.ndarray,
+               zs: np.ndarray,
+               intrinsic: np.ndarray = CAM_INTRINSIC,
+               depth_units_to_tracked_units: float = METERS_TO_METERS) -> np.ndarray:
+    fu = intrinsic[0, 0]
+    fv = intrinsic[1, 1]
+
+    center_u = intrinsic[0, 2]
+    center_v = intrinsic[1, 2]
+
+    constant_u = 1.0 / fu
+    constant_v = 1.0 / fv
+    xs = (us - center_u) * zs * depth_units_to_tracked_units * constant_u
+    ys = (vs - center_v) * zs * depth_units_to_tracked_units * constant_v
+    zs = zs * depth_units_to_tracked_units
+
+    xyzs_img = np.stack((xs, ys, zs), axis=1)
+    print("xyzs_img shape:", xyzs_img.shape)
+    # xyzs_cam = (H_IMG_TO_CAM @ np.hstack([xyzs_img, np.ones((xyzs_img.shape[0], 1))]).T).T[:, :-1]
+    xyzs_cam = tform_coords(H_IMG_TO_CAM, xyzs_img)
+    return xyzs_cam
+
+
+def xyz_to_uvz(
+        xyz: np.ndarray,
+        is_rounding_to_int: bool = False,
+        intrinsic: np.ndarray = CAM_INTRINSIC,
+        depth_units_to_tracked_units: float = METERS_TO_METERS) -> Tuple[np.ndarray, np.ndarray]:
+    xyz = tform_coords(np.linalg.inv(H_IMG_TO_CAM), xyz)
+    fx = intrinsic[0, 0]
+    fy = intrinsic[1, 1]
+
+    center_x = intrinsic[0, 2]
+    center_y = intrinsic[1, 2]
+
+    constant_x = 1.0 / fx
+    constant_y = 1.0 / fy
+
+    # xyz = xyz.T
+    xs = xyz[:, 0]
+    ys = xyz[:, 1]
+    zs = xyz[:, 2]
+    zs = zs / depth_units_to_tracked_units
+
+    us = xs / (zs * depth_units_to_tracked_units * constant_x) + center_x
+    vs = ys / (zs * depth_units_to_tracked_units * constant_y) + center_y
+
+    if is_rounding_to_int:
+        us = np.round(us).astype(int)
+        vs = np.round(vs).astype(int)
+
+    # uv_coords = np.round(np.stack((us, vs), axis=0)).astype(int)
+
+    return us, vs, zs
 
 
 def imgs_to_clouds_np(rgb_img: np.ndarray,
@@ -30,14 +98,14 @@ def imgs_to_clouds_np(rgb_img: np.ndarray,
     This has a lot of commented code that can be used to filter out points given a 2D (segmentation)
     mask. I don't think we'll need this but I'm keeping it in case we do.
     """
-    fx = intrinsic[0, 0]
-    fy = intrinsic[1, 1]
+    # fx = intrinsic[0, 0]
+    # fy = intrinsic[1, 1]
 
-    center_x = intrinsic[0, 2]
-    center_y = intrinsic[1, 2]
+    # center_x = intrinsic[0, 2]
+    # center_y = intrinsic[1, 2]
 
-    constant_x = 1.0 / fx
-    constant_y = 1.0 / fy
+    # constant_x = 1.0 / fx
+    # constant_y = 1.0 / fy
 
     U, V = get_uv_coords(depth_img.shape[0], depth_img.shape[1])
 
@@ -50,9 +118,11 @@ def imgs_to_clouds_np(rgb_img: np.ndarray,
     depth_flat = depth_img.flatten()
 
     # Get individual values.
-    xs = (U - center_x) * depth_flat * depth_units_to_tracked_units * constant_x
-    ys = (V - center_y) * depth_flat * depth_units_to_tracked_units * constant_y
-    zs = depth_flat * depth_units_to_tracked_units
+    # xs = (U - center_x) * depth_flat * depth_units_to_tracked_units * constant_x
+    # ys = (V - center_y) * depth_flat * depth_units_to_tracked_units * constant_y
+    # zs = depth_flat * depth_units_to_tracked_units
+    xyz = uvz_to_xyz(U, V, depth_flat, intrinsic, depth_units_to_tracked_units)
+    xs, ys, zs = xyz[:, 0], xyz[:, 1], xyz[:, 2]
 
     # Z = 0 indicates the point is invalid in the depth images that I've been working with in the
     # lab. I'm not sure if TartanAir has a similar convention.
@@ -87,36 +157,38 @@ def imgs_to_clouds_np(rgb_img: np.ndarray,
     return PointCloud(xyz_cloud_unfiltered.T, rgb_cloud_unfiltered.T), corner_idxs
 
 
-def cloud_to_img_np(
-        cloud: PointCloud,
-        intrinsic: np.ndarray,
-        img_width: int = 640,
-        img_height: int = 480,
-        depth_units_to_tracked_units: float = METERS_TO_METERS,
-        corner_idxs: Optional[List[int]] = None,
-        interpolation_method: Optional[str] = "clough_tocher") -> Tuple[np.ndarray, BoundingBox]:
+def cloud_to_img_np(cloud: PointCloud,
+                    intrinsic: np.ndarray,
+                    img_width: int = 640,
+                    img_height: int = 480,
+                    depth_units_to_tracked_units: float = METERS_TO_METERS,
+                    corner_idxs: Optional[List[int]] = None,
+                    interpolation_method: Optional[str] = None) -> Tuple[np.ndarray, BoundingBox]:
     """Turns cloud coordinates to UV (image) coordinates
 
     TODO: Do we need to do some sort of bilinear interpolation here to correct for camera
     distortions? While working with images from real sensors, I've seen that reprojection from 3D to
     2D rasterized images can leave holes in the image due to camera distortions.
     """
-    fx = intrinsic[0, 0]
-    fy = intrinsic[1, 1]
+    # fx = intrinsic[0, 0]
+    # fy = intrinsic[1, 1]
 
-    center_x = intrinsic[0, 2]
-    center_y = intrinsic[1, 2]
+    # center_x = intrinsic[0, 2]
+    # center_y = intrinsic[1, 2]
 
-    constant_x = 1.0 / fx
-    constant_y = 1.0 / fy
+    # constant_x = 1.0 / fx
+    # constant_y = 1.0 / fy
 
-    xs = cloud.xyz[:, 0]
-    ys = cloud.xyz[:, 1]
-    zs = cloud.xyz[:, 2] / depth_units_to_tracked_units
+    # xs = cloud.xyz[:, 0]
+    # ys = cloud.xyz[:, 1]
+    # zs = cloud.xyz[:, 2] / depth_units_to_tracked_units
 
-    vs = xs / (zs * depth_units_to_tracked_units * constant_x) + center_x
-    us = ys / (zs * depth_units_to_tracked_units * constant_y) + center_y
+    # vs = xs / (zs * depth_units_to_tracked_units * constant_x) + center_x
+    # us = ys / (zs * depth_units_to_tracked_units * constant_y) + center_y
 
+    us, vs, zs = xyz_to_uvz(cloud.xyz,
+                            intrinsic=intrinsic,
+                            depth_units_to_tracked_units=depth_units_to_tracked_units)
     uv_coords = np.round(np.stack((us, vs), axis=0)).astype(int)
 
     if corner_idxs is not None:
@@ -133,6 +205,7 @@ def cloud_to_img_np(
     where_uv_valid = np.logical_and(where_u_valid, where_v_valid)
 
     uv_coords = uv_coords[:, where_uv_valid]
+    zs = zs[where_uv_valid]
     rgb_valid = cloud.rgb[where_uv_valid, :]
 
     # We could try to interpolate the non-rasterized points into the rasterized grid (I think numpy
@@ -140,7 +213,11 @@ def cloud_to_img_np(
     if interpolation_method is None:
         img = np.zeros((img_height, img_width, 3), dtype=np.uint8)
         img[uv_coords[0], uv_coords[1], :] = rgb_valid
+
+        depth_img = np.zeros((img_height, img_width), dtype=np.float32)
+        depth_img[uv_coords[0], uv_coords[1]] = zs
     elif interpolation_method == "clough_tocher":
+        raise NotImplementedError("Clough-Tocher interpolation not yet implemented.")
         from scipy.interpolate import CloughTocher2DInterpolator
         # from scipy.interpolate import LinearNDInterpolator
         uv_coords = uv_coords.T
@@ -177,7 +254,7 @@ def cloud_to_img_np(
     #     rgb = cloud.rgb[i, :]
     #     img[u, v, :] = rgb
 
-    return img, valid_bbox
+    return img, depth_img, valid_bbox
 
 
 def transform_cloud(cloud: PointCloud, H: np.ndarray) -> PointCloud:
@@ -185,10 +262,10 @@ def transform_cloud(cloud: PointCloud, H: np.ndarray) -> PointCloud:
     cloud_tformed = copy.deepcopy(cloud)
 
     # Augment the xyz cloud with 1s so transform matmul works.
-    xyz_aug = np.hstack([cloud_tformed.xyz, np.ones((cloud_tformed.xyz.shape[0], 1))])
+    # xyz_aug = np.hstack([cloud_tformed.xyz, np.ones((cloud_tformed.xyz.shape[0], 1))])
 
-    xyz_tformed = (H @ xyz_aug.T).T
-    cloud_tformed.xyz = xyz_tformed[:, :-1]
+    # xyz_tformed = (H @ xyz_aug.T).T
+    cloud_tformed.xyz = tform_coords(H, cloud_tformed.xyz)
     return cloud_tformed
 
 
@@ -201,11 +278,12 @@ def reproject_img(rgb_1: np.ndarray,
     cloud_frame_1, corner_idxs = imgs_to_clouds_np(rgb_1, depth_1, CAM_INTRINSIC)
     H_1_2 = np.linalg.inv(pose_1) @ pose_2
     cloud_tformed = transform_cloud(cloud_frame_1, H_1_2)
-    img_tformed, valid_bbox = cloud_to_img_np(cloud_tformed,
-                                              CAM_INTRINSIC,
-                                              corner_idxs=corner_idxs,
-                                              interpolation_method=interpolation_method)
-    return img_tformed, valid_bbox
+    img_tformed, depth_tformed, valid_bbox = cloud_to_img_np(
+        cloud_tformed,
+        CAM_INTRINSIC,
+        corner_idxs=corner_idxs,
+        interpolation_method=interpolation_method)
+    return img_tformed, depth_tformed, valid_bbox
 
 
 def imgs_to_clouds_torch(
