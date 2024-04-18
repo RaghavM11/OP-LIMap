55193ffb1e2949fa724987e1a9a15a87685c7180
Dylan Colli <dylanfrankcolli@gmail.com>
2024-04-18 07:45:57 -0400

    Add image direction enums for easier input/output

--- limap_extension/constants.py
+++ limap_extension/constants.py
@@ -1,4 +1,19 @@
+from enum import Enum
+
 import numpy as np
+
+
+class ImageType(Enum):
+    """Enum for the different types of images."""
+    DEPTH = "depth"
+    IMAGE = "image"
+
+
+class ImageDirection(Enum):
+    """Enum for the directions images were taken in (left/right)"""
+    LEFT = "left"
+    RIGHT = "right"
+
 
 # yapf: disable
 CAM_INTRINSIC = np.array((
@@ -6,29 +21,17 @@ CAM_INTRINSIC = np.array((
     (  0, 320, 240),
     (  0,   0,   1)), dtype=float)
 
-# H_IMG_TO_CAM = np.array((
-#     (0, 0, -1, 0),
-#     (0, 1, 0, 0),
-#     (-1, 0, 0, 0),
-#     (0, 0, 0, 1)
-# ))
-# H_IMG_TO_CAM = np.array((
-#     (0, 0, 1, 0),
-#     (1, 0, 0, 0),
-#     (0, 1, 0, 0),
-#     (0, 0, 0, 1)
-# ))
 H_CAM_TO_NED = np.array((
     (0, 1, 0, 0),
     (0, 0, 1, 0),
     (1, 0, 0, 0),
     (0, 0, 0, 1)
 ), dtype=np.float32)
+
 H_NED_TO_CAM = np.array((
     (0, 0, 1, 0),
     (1, 0, 0, 0),
     (0, 1, 0, 0),
     (0, 0, 0, 1)
 ), dtype=np.float32)
-# H_IMG_TO_CAM = np.eye(4)
 # yapf: enable
