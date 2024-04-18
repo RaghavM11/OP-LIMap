004fdef53c3365a89b73814e2e2acf9364badf85
RaghavM11 <mishraraghav111@gmail.com~>
2024-04-18 12:45:55 -0400

    depthloader modified

---
+++ depth_api.py
@@ -0,0 +1,7 @@
+#auxillary depth API
+
+# from limap.runners.line_fitnmerge import line_fitnmerge
+
+# limap.runners.line_fitnmerge.line_fitnmerge(cfg, imagecols, depths, neighbors=None, ranges=None)
+
+
