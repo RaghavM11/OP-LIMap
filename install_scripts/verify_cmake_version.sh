2aff974b1bbffa3ffba86a2d97187ce7238f2a2c
Dylan Colli <dylanfrankcolli@gmail.com>
2024-04-20 06:00:41 -0400

    Add cmake verification and terrible colmap verification

--- install_scripts/verify_cmake_version.sh
+++ install_scripts/verify_cmake_version.sh
@@ -2,6 +2,7 @@
 
 # Function for checking the CMake version
 verify_cmake_version() {
+    echo "Checking CMake version..."
     CMAKE_VERSION=$(cmake --version)
 
     # Use grep with Perl-compatible regular expressions (-P) and only output the matched version number (-o)
@@ -32,29 +33,4 @@ verify_cmake_version() {
     fi
 }
 
-# Verify that the directory with which this bash script was executed from is the LIMap-Extension directory
-if [[ "$(basename "$(pwd)")" != "LIMap-Extension" ]]; then
-    echo "Please execute this script from the LIMap-Extension directory."
-    exit 1
-fi
-
-echo "Ensuring submodules are up to date..."
-git submodule update --init --recursive
-
-# TODO: Ensure colmap and poselib are installed.
-
-# TODO: Parse apt list --installed to ensure apt dependencies are installed
-# Perhaps by using a Python file that raises an exception if they aren't?
-
-# TODO: Ensure CMAKE version is at least 3.17
 verify_cmake_version
-
-# echo "Installing dependencies..."
-
-# This is really dissatisfying. I believe the issue is that the limap developers didn't specify a
-# required version of some of the third-party libraries they use, so the latest versions are
-# installed by default, which are incompatible with the limap code. We place these file overrides in
-# the limap directory to fix the issue.
-echo "Overriding limap files..."
-cp -r file_overrides/* .
-echo "Done"
