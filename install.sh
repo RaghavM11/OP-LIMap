ae9ef0d933c5ea6d05b330d6c2504f350abe5f96
Dylan Colli <dylanfrankcolli@gmail.com>
2024-04-20 05:10:52 -0400

    Verify CMAKE version in install script

--- install.sh
+++ install.sh
@@ -1,5 +1,45 @@
 #!/bin/bash
 
+echo "Ensuring submodules are up to date..."
+git submodule update --init --recursive
+
+# TODO: Ensure colmap and poselib are installed.
+
+# TODO: Parse apt list --installed to ensure apt dependencies are installed
+# Perhaps by using a Python file that raises an exception if they aren't?
+
+# TODO: Ensure CMAKE version is at least 3.17
+CMAKE_VERSION=$(cmake --version)
+
+echo "CMAKE version output $CMAKE_VERSION"
+
+# Use grep with Perl-compatible regular expressions (-P) and only output the matched version number (-o)
+kREGEX_EXPRESSION='version\s+\K[0-9]+\.[0-9]+(\.[0-9]+)?'
+version=$(echo "$CMAKE_VERSION" | grep -Po 'version\s+\K[0-9]+\.[0-9]+(\.[0-9]+)?')
+if [[ $version ]]; then
+    # Save the current IFS value so it can be restored later
+    OLDIFS=$IFS
+    # Set IFS to the delimiter (.)
+    IFS='.'
+    # Read the version into three separate variables
+    read -r CMAKE_MAJOR CMAKE_MINOR CMAKE_BUGFIX <<< "$version"
+    # Restore the original IFS value
+    IFS=$OLDIFS
+
+    if [[ $CMAKE_MAJOR -lt 3 ]]; then
+        echo "CMake version is less than 3.17. Please install CMake version 3.17 or greater."
+        exit 1
+    elif [[ $CMAKE_MAJOR -eq 3 && $CMAKE_MINOR -lt 17 ]]; then
+        echo "CMake version is less than 3.17. Please install CMake version 3.17 or greater."
+        exit 1
+    else
+        echo "CMake version is 3.17 or greater, CMake dependency is met."
+    fi
+else
+  echo "CMake version could not be determined."
+  exit 1
+fi
+
 # echo "Installing dependencies..."
 
 # This is really dissatisfying. I believe the issue is that the limap developers didn't specify a
