070c62a2aae01a6d6e46d1ead50e3d51e8577387
Dylan Colli <dylanfrankcolli@gmail.com>
2024-04-21 21:42:12 -0400

    First pass of joint point and line visualization

--- scripts/visualize_3d_lines.py
+++ scripts/visualize_3d_lines.py
@@ -38,6 +38,9 @@ def parse_to_config():
                             type=int,
                             default=2,
                             help='number of visible views')
+    arg_parser.add_argument('--visclouds',
+                            action="store_true",
+                            help="whether to visualize the point clouds")
     #arg_parser.add_argument('--bpt3d_pl', type=str, default=None, help=".npz file for point-line associations") #add for rerun point-line association
     #arg_parser.add_argument('--bpt3d_vp', type=str, default=None, help=".npz file for line-vanishing point associations") #add for rerun
     #arg_parser.add_argument('--segments2d', type=str, default=None, help="directory containing detected 2D segments")# add for rerun
@@ -78,12 +81,15 @@ def parse_to_config():
     args, unknown = parse_args(arg_parser)
     cfg = parse_config(args, unknown)
 
-    cfg["metainfos"] = os.path.join(cfg["output_dir"], cfg["experiment_name"], "metainfos.txt")
+    # cfg["metainfos"] = os.path.join(cfg["output_dir"], cfg["experiment_name"], "metainfos.txt")
+    cfg["metainfos"] = None
     cfg["imagecols"] = None
     cfg["use_robust_ranges"] = False
     cfg["mode"] = "rerun"
     cfg["scale"] = 1.0
     cfg["cam_scale"] = 1.0
+    # cfg["visclouds"] = args.visclouds
+    cfg["visclouds"] = True
     return cfg
 
 
@@ -134,6 +140,73 @@ def main(cfg):
         raise ValueError("Error! Track information is not available.")
     if cfg["imagecols"] is None:
         vis_3d_lines(lines, mode=cfg["mode"], ranges=ranges, scale=cfg["scale"])
+
+        if cfg["visclouds"]:
+            from limap_extension.point_cloud import PointCloud
+            from limap_extension.point_cloud_list import PointCloudList
+            from limap_extension.transforms_spatial import get_transform_matrix_from_pose_array
+            from limap_extension.visualization.rerun.figure_factory import FigureFactory
+            from limap_extension.constants import CAM_INTRINSIC
+            from limap_extension.constants import ImageDirection
+            from limap_extension.utils.io import read_all_rgbd, read_pose
+            from limap_extension.img_cloud_transforms import (reproject_img, uvz_ocv_to_xyz_ned,
+                                                              xyz_ned_to_uvz_ocv,
+                                                              index_img_with_uv_coords,
+                                                              get_uv_coords, imgs_to_clouds_np,
+                                                              ned2cam_single_pose, transform_cloud,
+                                                              inverse_pose)
+            from limap_extension.constants import H_NED_TO_OCV, H_OCV_TO_NED
+            trial_dir = cfg["extension_dataset"]["dataset_path"]
+            poses_world_frame = read_pose(trial_dir, ImageDirection.LEFT)
+
+            imgs, _, depths, _ = read_all_rgbd(trial_dir, ImageDirection.LEFT)
+            pcl = PointCloudList()
+            pc: PointCloud = None
+            for i, (img, depth) in enumerate(zip(imgs, depths)):
+                # Should truly be in camera frame (NED)
+                cloud, _ = imgs_to_clouds_np(img, depth, CAM_INTRINSIC)
+                cloud.normalize_rgb()
+
+                # May need to do inverse like original transform code.
+                # pose_cam_to_world = inverse_pose(ned2cam_single_pose(poses_world_frame[i]))
+                # pose_cam_to_world = get_transform_matrix_from_pose_array(poses_world_frame[i])
+                # pose_cam_to_world = H_OCV_TO_NED @ inverse_pose(
+                #     get_transform_matrix_from_pose_array(poses_world_frame[i])) @ H_NED_TO_OCV
+                # Looks good but is off from the lines almost like a reflection
+                # pose_cam_to_world = H_NED_TO_OCV @ inverse_pose(
+                #     get_transform_matrix_from_pose_array(poses_world_frame[i]))
+                pose_cam_to_world = H_OCV_TO_NED @ inverse_pose(
+                    get_transform_matrix_from_pose_array(poses_world_frame[i]))
+
+                cloud = transform_cloud(cloud, pose_cam_to_world)
+                # pcl.append(cloud)
+                if pc is None:
+                    pc = cloud
+                else:
+                    # Extend the point cloud
+                    pc.xyz = np.concatenate((pc.xyz, cloud.xyz), axis=0)
+                    print(pc.xyz.shape)
+                    pc.rgb = np.concatenate((pc.rgb, cloud.rgb), axis=0)
+
+                pc.downsample(0.10)
+
+            # pc = transform_cloud(pc, H_OCV_TO_NED)
+            # pc.xyz = tform_coords
+            # pc.xyz = np.dot(H_OCV_TO_NED[:3, :3], pc.xyz.T).T + H_OCV_TO_NED[:3, 3]
+
+            pcl.append(pc)
+            ff = FigureFactory()
+            ff.add_cloud("Clouds", pcl)
+            print(f"Scene bounds max: {ff._scene_bounds_max}")
+            print(f"Scene bounds min: {ff._scene_bounds_min}")
+            bounds = np.stack((ff._scene_bounds_min, ff._scene_bounds_max), axis=1)
+            # mean = np.mean(bounds, axis=1)
+            # std = np.std(bounds, axis=1)
+            # ff._update_bounds(mean - std, mean + std)
+            ff._update_bounds(np.array((-25, -25, -25)), np.array((25, 25, 25)))
+            # ff._scene_bounds_min = mean - 2 * std
+            # ff._scene_bounds_max = mean + 2 * std
+            ff.make_fig(do_init=False)
     else:
         # if (not cfg["imagecols"]):
         #     raise ValueError("Error! Input file {0} is not valid".format(cfg["imagecols"]))
